185e7a3688beb335f9c79d5ccfcecdb33e385452
Ashish <ashish@techkunzite.com>
2025-07-10 18:46:41 +0530

    do while loop - Print "Hello" 10 times

---
+++ loop/DoWhile.java
@@ -0,0 +1,10 @@
+public class DoWhile {
+    public static void main(String args[]){
+        // Q1. Print "Hello" 10 times
+        int i=1;
+        do{
+            System.out.println(i+" Hello");
+            i++;
+        }while(i<=10);
+    }
+}
