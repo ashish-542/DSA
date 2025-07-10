2a170807e4c71e8c0e141c650972895df666d83c
Ashish <ashish@techkunzite.com>
2025-07-10 18:14:12 +0530

    initial commit

---
+++ loop/While.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+public class While {
+    public static void main(String args[]){
+        // Q1. Print "Hello" 100 times
+        // int i=1;
+        // while(i<=100){
+        //     System.out.println(i+" Hello");
+        //     i++;
+        // }
+
+        // Q2. Print nos. from 1-100
+        // int i=1;
+        // while(i<=100){
+        //     System.out.println(i);
+        //     i++;
+        // }
+
+        // Q3. Print nos. from 1-n
+        // Scanner sc = new Scanner(System.in);
+        // System.out.print("Enter a no. - ");
+        // int n = sc.nextInt();
+        // int i=1;
+        // while(i<=n){
+        //     System.out.println(i);
+        //     i++;
+        // }
+
+        // Q4. Print the sum of 1st n natural nos.
+        // Scanner sc = new Scanner(System.in);
+        // System.out.print("Enter a no. - ");
+        // int n = sc.nextInt();
+
+        // int i=1;
+        // int sum=0;
+        // while(i<=n){
+        //     sum +=i;
+        //     i++;
+        // }
+        // System.out.println("Sum of 1st "+n+" Natural nos. = "+sum);
+    }
+}
