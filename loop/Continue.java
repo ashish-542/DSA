ba11efce612634b4387f2bee029375032fcf5e3a
Ashish <ashish@techkunzite.com>
2025-07-10 19:04:14 +0530

    continue statement - Print nos. from 1-5 but skip 3

---
+++ loop/Continue.java
@@ -0,0 +1,12 @@
+public class Continue {
+    public static void main(String args[]){
+        // Q1. Print nos. from 1-5 but skip 3
+        // for(int i=1;i<=5;i++){
+        //     if(i==3){
+        //         System.out.println("---- Iteration skipped ----");
+        //         continue;
+        //     }
+        //     System.out.println("No. is - "+i);
+        // }
+    }
+}
