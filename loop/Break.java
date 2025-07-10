799673f228aaa755b4cc0ff271dca1099f704994
Ashish <ashish@techkunzite.com>
2025-07-10 18:58:46 +0530

    break statement ques - Keep entering nos. till user enters a multiple of 10

--- loop/Break.java
+++ loop/Break.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class Break {
     public static void main(String args[]){
         // Q1. Print nos. from 1-5 but skip 3
@@ -7,6 +9,17 @@ public class Break {
         //     }
         //     System.out.println("No. is "+i);
         // }
-        
+
+        // Q2. Keep entering nos. till user enters a multiple of 10
+        while(true){
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Enter a no. - ");
+            int n = sc.nextInt();
+            if(n%10==0){
+                System.out.println("------ Loop break ------");
+                break;
+            }
+            System.out.println("No. is "+n);
+        }
     }
 }
