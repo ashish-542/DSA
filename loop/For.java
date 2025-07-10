91168cf0df96bc9faa204e898c14514fd440f3f9
Ashish <ashish@techkunzite.com>
2025-07-10 18:29:07 +0530

    for loop - sum of n N nos & print Hello 10 times

---
+++ loop/For.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class For{
+    public static void main(String args[]){
+        // Q1. Print "Hello" 10 times
+        // for(int i=1;i<=10;i++){
+        //     System.out.println(i+" Hello");
+        // }
+
+        // Q2. Print sum of first n Natural nos.
+        // Scanner sc = new Scanner(System.in);
+        // System.out.print("Enter the range - ");
+        // int n = sc.nextInt();
+        // int sum=0;
+        // for(int i=1;i<=n;i++){
+        //     sum +=i;
+        // }
+        // System.out.println("Sum of 1st "+n+" natural nos = "+sum);
+
+        
+    }
+}
