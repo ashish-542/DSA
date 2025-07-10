d207f8a52f80fc5870d4a6e6ea8d01ae3fca92dd
Ashish <ashish@techkunzite.com>
2025-07-10 18:51:45 +0530

    break statement - Print nos. from 1-5 but skip 3

---
+++ loop/Break.java
@@ -0,0 +1,12 @@
+public class Break {
+    public static void main(String args[]){
+        // Q1. Print nos. from 1-5 but skip 3
+        // for(int i=1;i<=5;i++){
+        //     if(i==3){
+        //         break;
+        //     }
+        //     System.out.println("No. is "+i);
+        // }
+        
+    }
+}
