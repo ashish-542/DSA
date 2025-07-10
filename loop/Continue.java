19ef7e22e804b8cb484f8582425292e3ebfa281f
Ashish <ashish@techkunzite.com>
2025-07-10 20:01:38 +0530

    continue statement - Display all nos. entered by user except multiples of 10

--- loop/Continue.java
+++ loop/Continue.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class Continue {
     public static void main(String args[]){
         // Q1. Print nos. from 1-5 but skip 3
@@ -7,6 +9,18 @@ public class Continue {
         //         continue;
         //     }
         //     System.out.println("No. is - "+i);
+        // }
+
+        // Q2. Display all nos. entered by user except multiples of 10
+        // Scanner sc = new Scanner(System.in);
+        // while(true){
+        //     System.out.print("Enter a no. - ");
+        //     int n = sc.nextInt();
+        //     if(n%10==0){
+        //         System.out.println("---- Iteration Skipped ----");
+        //         continue;
+        //     }
+        //     System.out.println("No. is "+n);
         // }
     }
 }
