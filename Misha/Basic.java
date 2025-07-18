0c056c4627fe953942f98991615aab892c8794a5
Ashish <ashish@techkunzite.com>
2025-07-18 12:32:39 +0530

    output statements

---
+++ Misha/Basic.java
@@ -0,0 +1,39 @@
+public class Basic {
+    public static void main(String agrs[]){
+        // Whole Java code will be inside it
+
+        // Output - print, println and \n
+        // System.out.print("Hello");
+        // System.out.println(" Misha !!");
+        // System.out.println(" ");
+        // System.out.print("How are you ? \n");
+        // System.out.print("Where do you live ?");
+
+        // Print this pattern:
+        // *****
+        // ****
+        // ***
+        // **
+        // *
+
+        // Sol 1 - using println
+
+        // System.out.println("*****");
+        // System.out.println("****");
+        // System.out.println("***");
+        // System.out.println("**");
+        // System.out.println("*");
+
+        // Sol 2 - using mulitple print and \n
+
+        // System.out.print("***** \n");
+        // System.out.print("**** \n");
+        // System.out.print("*** \n");
+        // System.out.print("** \n");
+        // System.out.print("* \n");
+
+        // Sol 3 - using single print and \n
+
+        // System.out.print("***** \n**** \n*** \n** \n*");
+    }
+}
