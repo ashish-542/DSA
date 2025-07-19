17286b1832f3419b28b15084cffd84f4e0d50d7f
Ashish <ashish@techkunzite.com>
2025-07-19 19:44:39 +0530

    data types & variables

---
+++ Misha/DataType.java
@@ -0,0 +1,68 @@
+public class DataType {
+    public static void main(String args[]){
+        // Data Type - The Type of value a variable will hold and how much memory it will cover.
+
+        // 2 Types:
+
+        // 1. Primitive (Primary) - Pre-defined.
+        // 2. Non-Primitive (Secondary) - Created as per our need.
+
+        // 1. Primitive - 8 data types
+
+        // A. byte - 1 byte (8 bits)
+
+        // byte b = 4;
+        // System.out.println("Byte data type - "+b);
+
+        // B. short - small nos. - 2 byte
+
+        // short s = 40;
+        // System.out.println("Short data type - "+s);
+
+        // C. char - characters - A-Z, a-z, 0-9 or special characters (*, #, %, @, =) - 2 bytes
+        // Use single quotes and just single alphabet/digit or special character
+
+        // char c1 = 'A';
+        // System.out.println("Char data type - "+c1);
+
+        // char c2 = 'b';
+        // System.out.println("Char data type - "+c2);
+
+        // char c3 = '1';
+        // System.out.println("Char data type - "+c3);
+
+        // char c4 = '#';
+        // System.out.println("Char data type - "+c4);
+
+        // 4. Boolean - true or false - 1 byte
+
+        // boolean b = true;
+
+        // System.out.println("Boolean Data Type "+b);
+
+        // 5. Interger - nos. - int - 4 bytes
+
+        // int n = 20;
+
+        // System.out.println("Interger data type - "+n);
+
+        // 6. Long - larger nos. - long - 8 bytes
+
+        // long n = 20;
+
+        // System.out.println("Long data type - "+n);
+
+        // 7. Float - nos. with decimal point - float - 4 bytes
+
+        // float n = 20.89f;
+
+        // System.out.println("Float data type - "+n);
+
+        // 8. Double - larger nos. with decimal point - double - 8 bytes
+
+        // double n = 20.89;
+
+        // System.out.println("Double data type - "+n);
+
+    }
+}
---
+++ Misha/Variable.java
@@ -0,0 +1,11 @@
+public class Variable {
+    public static void main(String args[]){
+        // Variable - vary (Change) - A container to store some value. It may change from time to time.
+
+        // Syntax
+        // data_type variable_name = value;
+
+        
+    
+    }
+}
