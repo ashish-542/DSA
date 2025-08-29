85aedd4954d4692bd7eb99028265a2450ae63c8d
Ashish <ashish@techkunzite.com>
2025-08-29 15:20:01 +0530

    Character Pattern

---
+++ loop/NestedLoops/CharacterPattern.java
@@ -0,0 +1,19 @@
+public class CharacterPattern{
+    public static void main(String args[]){
+
+        // Q. print
+        //     A
+        //     BC
+        //     DEF
+        //     GHIJ
+
+        char ch = 'A';
+        for(int i=1;i<=4;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(ch);
+                ch++;
+            }
+            System.out.println();
+        }
+    }
+}
